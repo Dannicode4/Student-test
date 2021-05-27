d2a4fdbbe48df820f2a6da38bfe3be4a3dd2a8cf
enriq <enriq@DESKTOP-HU6HRBE.cogeco.local>
2021-05-27 09:20:35 -0400

    initial commit

---
+++ src/GitTest.java
@@ -0,0 +1,13 @@
+/**
+ * Daniel Enriquez "test"
+ */
+public class GitTest
+{
+
+
+   public static void main (String[] args)
+   {
+      int a = 1;
+      System.out.println("this is the " + a + "st Github test");
+   }
+}
